78d0c6f450ae720944bb4debd8a5a96d0d93eae1
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2023-12-03 18:11:56 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,4 @@
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
-import ViteComponents from 'vite-plugin-components';
 import { resolve } from 'path';
 
 export default defineConfig({
